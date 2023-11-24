6a764b2971a1cd2510e69332703ed76e5cd4d02c
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2023-11-24 14:21:25 +0300

    Display pygame inside tk

--- src/sabkra/src/gui.py
+++ src/sabkra/src/gui.py
@@ -2,15 +2,20 @@ import tkinter as tk
 from tkinter import filedialog as fd
 from . import world_display
 from threading import Thread
+import os
+import platform
 
 root = tk.Tk()
+root.title("Sabkra - Civ 5 World previewer")
 
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
-window_width = 400
-window_height = screen_height
+window_width = screen_width
+window_height = screen_height - 50
 
-world_display.default_window_width = screen_width - window_width
+sidebar_width = 500
+
+world_display.default_window_width = screen_width
 world_display.default_window_height = screen_height - 50
 
 # calculate x and y coordinates for the Tk root window
@@ -36,7 +41,7 @@ def select_file():
 
     filename = fd.askopenfilename(
         title='Open',
-        initialdir=r"~/.local/share/Aspyr/Sid Meier's Civilization 5/",
+        initialdir=r"~/.local/share/Aspyr/Sid Meier's Civilization 5/MODS/gedemon's ynaemp (v 25)/",
         filetypes=filetypes)
 
     p = Thread(target=world_display.display_world, args=(filename, tile_label))
@@ -45,13 +50,36 @@ def select_file():
     print('got here')
 
 
-open_button = tk.Button(
-    root,
-    text='Open',
+menu = tk.Menu(root)
+root.config(menu=menu)
+menu.add_command(
+    label='Open',
     command=select_file
 )
-open_button.pack()
-tile_label = tk.Label(root, text='')
-tile_label.pack()
+pygame_frame = tk.Frame(
+    root,
+    width=window_width - sidebar_width,
+    height=window_height,
+    highlightbackground='#595959',
+    highlightthickness=2,
+)
+sidebar_frame = tk.Frame(
+    root,
+    width=sidebar_width,
+    height=window_height,
+    highlightbackground='#595959',
+    highlightthickness=2,
+)
+tile_label = tk.Message(sidebar_frame, text='')
+tile_label.pack(fill=None, expand=False)
+pygame_frame.pack(side="left", fill="both")
+sidebar_frame.pack(side="right", fill="both")
+
+os.environ['SDL_WINDOWID'] = str(pygame_frame.winfo_id())
+system = platform.system()
+if system == "Windows":
+    os.environ['SDL_VIDEODRIVER'] = 'windib'
+elif system == "Linux":
+    os.environ['SDL_VIDEODRIVER'] = 'x11'
 
 root.mainloop()
