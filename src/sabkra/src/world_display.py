5530840d380cd352b49c820583195adf085631e1
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2023-11-25 18:00:58 +0300

    Rename wb to sc

--- src/sabkra/src/world_display.py
+++ src/sabkra/src/world_display.py
@@ -10,27 +10,27 @@ fps = 60
 
 
 def display_world_tk(file_path, ui, frame):
-    wb = Scene(file_path, ui)
+    sc = Scene(file_path, ui)
 
     def on_click(event):
-        wb.set_current_tile_to_mouse((event.x, event.y))
-        wb.draw()
+        sc.set_current_tile_to_mouse((event.x, event.y))
+        sc.draw()
 
     def on_motion(event):
-        wb.mouse.update(event.x, event.y)
+        sc.mouse.update(event.x, event.y)
 
     def on_drag(event):
-        wb.mouse.update(event.x, event.y)
-        wb.camera.drag(wb.mouse.vector())
-        wb.draw()
+        sc.mouse.update(event.x, event.y)
+        sc.camera.drag(sc.mouse.vector())
+        sc.draw()
 
     def on_zoom_in(event):
-        wb.camera.clean_rescale(1)
-        wb.draw()
+        sc.camera.clean_rescale(1)
+        sc.draw()
 
     def on_zoom_out(event):
-        wb.camera.clean_rescale(-1)
-        wb.draw()
+        sc.camera.clean_rescale(-1)
+        sc.draw()
 
     frame.bind('<Button-1>', on_click)
     frame.bind('<B3-Motion>', on_drag)
@@ -39,11 +39,11 @@ def display_world_tk(file_path, ui, frame):
     frame.bind("<Button-4>", on_zoom_in)
     frame.bind("<Button-5>", on_zoom_out)
 
-    wb.draw()
+    sc.draw()
 
 
 def display_world(file_path, ui):
-    wb = Scene(file_path, ui)
+    sc = Scene(file_path, ui)
 
     clock = pygame.time.Clock()
     drag_mode = False
@@ -58,10 +58,10 @@ def display_world(file_path, ui):
                 run = False
             if event.type == pygame.VIDEORESIZE:
                 # Resize the window
-                wb.on_resize_window(event.w, event.h)
+                sc.on_resize_window(event.w, event.h)
             if event.type == pygame.MOUSEWHEEL:
                 # Wheel scroll
-                wb.camera.clean_rescale(event.y)
+                sc.camera.clean_rescale(event.y)
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     # Right click
@@ -72,12 +72,12 @@ def display_world(file_path, ui):
             if event.type == pygame.MOUSEBUTTONUP:
                 if event.button == 1:
                     # Right click ended
-                    wb.set_current_tile_to_mouse(event.pos)
+                    sc.set_current_tile_to_mouse(event.pos)
                 else:
                     # Left click / wheel click ended
                     drag_mode = False
         if drag_mode:
-            wb.camera.drag(wb.mouse.vector())
-        wb.mouse.update(*pygame.mouse.get_pos())
-        wb.draw()
+            sc.camera.drag(sc.mouse.vector())
+        sc.mouse.update(*pygame.mouse.get_pos())
+        sc.draw()
     pygame.display.quit()
