f7f6cbf38797f035eb380fa1eb539218479f2eb9
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2023-12-04 16:09:33 +0300

    Parse players

--- src/sabkra/src/parser/player.py
+++ src/sabkra/src/parser/player.py
@@ -7,16 +7,57 @@ from .utils import (
     get_short,
     get_string,
     get_string_array,
+    get_buffer,
+    get_buffered_string,
 )
 
 
 @dataclass
 class Player:
-    struct: str
+    policies: bytes
+    leader_name: bytes
+    civ_name: bytes
+    civ_type: bytes
+    team_color: bytes
+    era: bytes
+    handicap: bytes
+    culture: int
+    gold: int
+    start_x: int
+    start_y: int
+    team: int
+    playable: int
 
     @classmethod
     def from_file(cls, f):
-        struct = "".join(map(chr, [get_byte(f) for _ in range(436)]))
+        policies = get_buffer(f, 32)
+        leader_name = get_buffered_string(f, 64)
+        civ_name = get_buffered_string(f, 64)
+        civ_type = get_buffered_string(f, 64)
+        team_color = get_buffered_string(f, 64)
+        era = get_buffered_string(f, 64)
+        handicap = get_buffered_string(f, 64)
+        culture = get_int(f)
+        gold = get_int(f)
+        start_x = get_int(f)
+        start_y = get_int(f)
+        team = get_byte(f)
+        playable = get_byte(f)
+        get_byte(f)
+        get_byte(f)
+
         return cls(
-            struct,
+            policies,
+            leader_name,
+            civ_name,
+            civ_type,
+            team_color,
+            era,
+            handicap,
+            culture,
+            gold,
+            start_x,
+            start_y,
+            team,
+            playable,
         )
